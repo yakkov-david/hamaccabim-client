8aa3010dabe311c2028dc97d7bfb11f22c46e4de
Yakkov david <yakkovdavid77@gmail.com>
2024-08-28 13:35:21 +0300

    changes

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 
 const config = {
-    //apiUrl: ' https://ef0a-46-19-86-111.ngrok-free.app',
+    apiUrl: 'https://3f3d-77-137-65-155.ngrok-free.app/',
     //apiUrl: 'http://localhost:3030',
-    apiUrl: 'https://hamaccabim-server-3271aad3f42b.herokuapp.com/',
+    //apiUrl: 'https://hamaccabim-server-3271aad3f42b.herokuapp.com/',
     apiKey: process.env.REACT_APP_API_KEY,
     // other configs
 };
