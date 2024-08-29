a33ebaea5976719a4825817e1ee58a35673151ad
Yakkov david <yakkovdavid77@gmail.com>
2024-08-29 21:24:29 +0300

    a

--- src/hamaccabim/CountdownPage.tsx
+++ src/hamaccabim/CountdownPage.tsx
@@ -8,7 +8,7 @@ import { useParams } from 'react-router-dom';
 const CountdownPage: React.FC = () => {
     const [eventDate, setEventDate] = useState("");
     const [pageTitle, setPageTitle] = useState("");
-    //const [imageUrl, setImageUrl] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
     const [Paragraph1, setParagraph1] = useState<JSX.Element[]>([]);
     const [Paragraph2, setParagraph2] = useState<JSX.Element[]>([]);
     const [Paragraph3, setParagraph3] = useState<JSX.Element[]>([]);
@@ -29,6 +29,7 @@ const CountdownPage: React.FC = () => {
             setParagraph2(convertNewLinesToJSX("באתר זה נפרסם כל פעם דפים של מסיתים וביחד נכנס לדפים אלו, נתלונן על אותם אנשים ונגרום לכך שהרשתות החברתיות יחסמו להם את הדפים."));
             setParagraph3(convertNewLinesToJSX("בואו נתאגד ונפעל למען מדינת ישראל."));
             setTwitterLink("https://twitter.com/jacksonhinklle");
+            setImageUrl("./images/hamaccabim-image.jpeg")
         };
 
         updatePageContent(); // Call the temporary function
@@ -173,6 +174,207 @@ const CountdownPage: React.FC = () => {
                         {Paragraph1}
                     </h2>
                 </p>
+                {imageUrl && <img src={imageUrl} alt="Event" width={800} height={200}/*className='img'*/ />}
+                <p>
+                    <h2>
+                        {Paragraph2}
+                    </h2>
+                </p>
+                {/*{name && <p><strong>{name}</strong></p>}*/}
+
+                {/*<a
+                    href={twitterLink}
+                    className="red-button"
+                    onClick={handleReportClick}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    REPORT
+                </a>*/}
+                <ResponseDialog open={dialogOpen} onClose={handleCloseDialog} />
+            </div>
+            <div className="footer" dir='rtl'>
+                <p>
+                    <h2>
+                        {Paragraph3}
+                    </h2>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default CountdownPage;
+
+
+
+
+
+
+
+
+/*import React, { useState, useEffect } from 'react';
+import config from '../config';
+import CountdownTimer from './CountdownTimer';
+import './CountdownLayout.css';
+import ResponseDialog from './ResponseDialog';
+import { useParams } from 'react-router-dom';
+
+const CountdownPage: React.FC = () => {
+    const [eventDate, setEventDate] = useState("");
+    const [pageTitle, setPageTitle] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [Paragraph1, setParagraph1] = useState<JSX.Element[]>([]);
+    const [Paragraph2, setParagraph2] = useState<JSX.Element[]>([]);
+    const [Paragraph3, setParagraph3] = useState<JSX.Element[]>([]);
+    const [name, setName] = useState(""); // New state for name
+    const [twitterLink, setTwitterLink] = useState(""); // New state for Twitter link
+
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const { documentId } = useParams<{ documentId: string }>();
+
+    useEffect(() => {
+
+
+
+
+
+        const fetchCountdownData = async () => {
+            if (documentId) {
+                try {
+                    const headers = new Headers();
+                    headers.set('Content-Type', 'application/json');
+                    if (config.apiKey) {
+                        headers.set('x-api-key', config.apiKey);
+                        headers.set('Access-Control-Allow-Origin', '*')
+                    }
+
+                    const response = await fetch(config.apiUrl + `/landing-pages/${documentId}`, {
+                        headers: headers,
+                    });
+
+                    if (response.ok) {
+                        const data = await response.json();
+                        console.log("Fetched data:", data);  // Debugging log
+
+                        setEventDate(data.CountdownDate);
+                        setPageTitle(data.title);
+                        setParagraph1(convertNewLinesToJSX(data.paragraph1));
+                        setParagraph2(convertNewLinesToJSX(data.paragraph2));
+                        setParagraph3(convertNewLinesToJSX(data.paragraph3));
+                        setImageUrl(data.ImageUrl);
+                        setName(data.name);
+                        setTwitterLink(data.twitterLink);
+                    } else {
+                        throw new Error('Failed to fetch countdown data');
+                    }
+                } catch (error) {
+                    console.error('Error fetching countdown data:', error);
+                }
+            }
+        };
+        fetchCountdownData();
+    }, [documentId]);
+
+    useEffect(() => {
+        reportVisit();
+    }, []);
+
+    const reportVisit = async () => {
+        try {
+            const headers = new Headers();
+            headers.set('Content-Type', 'application/json');
+            if (config.apiKey) {
+                headers.set('x-api-key', config.apiKey);
+                headers.set('Access-Control-Allow-Origin', '*')
+            }
+
+            await fetch(config.apiUrl + '/analytics', {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({
+                    event: 'visit',
+                    timestamp: new Date().toISOString(),
+                })
+            });
+        } catch (error) {
+            console.error('Error reporting visit:', error);
+        }
+    };
+
+    const convertNewLinesToJSX = (text: string | undefined): JSX.Element[] => {
+        if (!text) {
+            return []; // Return an empty array if the text is undefined or empty
+        }
+        return text.split('\n').map((line, index, array) => (
+            index === array.length - 1 ? <React.Fragment key={index}>{line}</React.Fragment> : <React.Fragment key={index}>{line}<br /></React.Fragment>
+        ));
+    };
+
+
+    const handleReportClick = async () => {
+        try {
+            const headers = new Headers();
+            headers.set('Content-Type', 'application/json');
+            if (config.apiKey) {
+                headers.set('x-api-key', config.apiKey);
+                headers.set('Access-Control-Allow-Origin', '*')
+            }
+
+            const response = await fetch(config.apiUrl + '/ip', {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({
+                    text: "Report content or any relevant information"
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            } else if (!response.headers.get('content-type')?.includes('application/json')) {
+                const responseBody = await response.text();
+                throw new Error(`Expected JSON but received: ${responseBody}`);
+            }
+
+            const data = await response.json();
+            console.log('Report successful:', data);
+
+            await fetch(config.apiUrl + '/analytics', {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify({
+                    event: 'click',
+                    timestamp: new Date().toISOString()
+                })
+            });
+
+        } catch (error) {
+            console.error('Error reporting:', error);
+        }
+
+        setDialogOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setDialogOpen(false);
+    };
+
+    return (
+        <div className="countdown-page">
+            <div className="header" dir="rtl">
+                <div>
+                    <h1>
+                        {pageTitle}
+                    </h1>
+                </div>
+            </div>
+            <div className="main-content" dir='rtl'>
+                <p className='p'>
+                    <h2>
+                        {Paragraph1}
+                    </h2>
+                </p>
                 <div dir='ltr'>
                     <CountdownTimer targetDate={new Date(eventDate)} />
                     <hr className="horizontal-line" />
@@ -182,8 +384,8 @@ const CountdownPage: React.FC = () => {
                         {Paragraph2}
                     </h2>
                 </p>
-                {/*{name && <p><strong>{name}</strong></p>}*/}
-                {/*{imageUrl && <img src={imageUrl} alt="Event" className='img' />}*/}
+                {name && <p><strong>{name}</strong></p>}
+                {imageUrl && <img src={imageUrl} alt="Event" className='img' />}
                 <a
                     href={twitterLink}
                     className="red-button"
@@ -206,4 +408,4 @@ const CountdownPage: React.FC = () => {
     );
 };
 
-export default CountdownPage;
+export default CountdownPage;*/
